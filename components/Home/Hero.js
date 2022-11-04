e0dd13f17c4a84747bd98a1e22bab6ff34f87e69
jonatanare <jonatanare.dev@gmail.com>
2022-11-04 13:57:28 -0500

    [FEATURE]: add navbar mobile, and hero section en mobile and desktop version

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -23,7 +23,7 @@ export default function Hero() {
     setDateNow(date);
   }, [dateNow]);
 
-  const [age, setAge] = useState('');
+  const [age, setAge] = useState("");
 
   const handleChange = (event) => {
     setAge(event.target.value);
@@ -33,59 +33,73 @@ export default function Hero() {
     <div className="container">
       <div className="row">
         <div className="col-12 ">
-          <div className="hero-section__content text-center">
-            <h1 className="mv-h1">Explora Cancún</h1>
-            <p className="hero-section__content--subtitle">
+          <div className="hero-section__content text-center text-lg-start">
+            <h1 className="mv-h1 hero-section__content--title">
+              Explora Cancún
+            </h1>
+            <p className="hero-section__content--subtitle mb-0">
+              Donde la belleza y la cultura se encuentran <br />
               Donde la belleza y la cultura se encuentran
             </p>
-            <button className="btn btn-movebike contained">
+            <button className="btn btn-movebike contained btn-cta">
               ¡Reserva Ahora!
             </button>
           </div>
           <div className="wrapper-booking">
-            <form
-              className="row g-3 wrapper-booking__card p-2 text-center"
-              action=""
-            >
-              <div className="col-12">
-                <div>
+            <form className="row g-3  p-2 text-center">
+              <div className="col-12 wrapper-booking__card ">
+                <div className="wrapper-booking__card--search-inputs">
                   <FormControl fullWidth>
-                  <InputLabel id="demo-simple-select-label">Ubicación</InputLabel>
+                    <InputLabel id="demo-simple-select-label">
+                      Ubicación
+                    </InputLabel>
                     <Select
                       labelId="demo-simple-select-label"
                       id="demo-simple-select"
                       value={age}
-                      className="datepicker mb-2"
+                      className="datepicker mb-2 mb-lg-0"
                       label="Ubicación"
                       onChange={handleChange}
-                      
                     >
                       <MenuItem value={10}>Cancún, Quintana, Roo.</MenuItem>
                       <MenuItem value={20}>Tulúm, Quintana, Roo.</MenuItem>
                     </Select>
                   </FormControl>
+                  <div className="separator"></div>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <FormControl fullWidth>
+                      <DatePicker
+
+                        label="Check In"
+                        className="datepicker mt-lg-none"
+                        value={checkIn}
+                        minDate={dayjs(dateNow)}
+                        onChange={(newValue) => {
+                          setCheckIn(newValue);
+                        }}
+                        renderInput={(params) => (
+                          <TextField {...params} helperText={null} />
+                        )}
+                      />
+                    </FormControl>
+                    <div className="separator"></div>
+                    <FormControl fullWidth>
+                      <DatePicker
+                      label={"Check Out"}
+                        className="datepicker mt-2 mt-lg-0"
+                        value={checkOut}
+                        minDate={dayjs(dateNow)}
+                        onChange={(newValue) => {
+                          setCheckOut(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                      />
+                    </FormControl>
+                  </LocalizationProvider>
                 </div>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    className="datepicker w-100"
-                    value={checkIn}
-                    minDate={dayjs(dateNow)}
-                    onChange={(newValue) => {
-                      setCheckIn(newValue);
-                    }}
-                    renderInput={(params) => <TextField {...params} helperText={null} />}
-                  />
-                  <DatePicker
-                    className="datepicker w-100 mt-2"
-                    value={checkOut}
-                    minDate={dayjs(dateNow)}
-                    onChange={(newValue) => {
-                      setCheckOut(newValue);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-                <button className="btn btn-movebike contained w-50 mt-3">
+              </div>
+              <div className="col-12">
+                <button className="btn btn-movebike contained btn-datapicker w-50 mt-3 mt-lg-0">
                   Buscar
                 </button>
               </div>
