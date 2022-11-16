b4497869f65ea24170500dab1d8fab6d831a68c9
jonatanare <jonatanare.dev@gmail.com>
2022-11-16 15:35:33 -0500

    add new confing en next.config file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false }
-
-    return config
+  webpack: (config,{ isServer } ) => {
+    config.resolve.fallback = { fs: false },
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg|webp)$/i,
+      loader: "url-loader",
+      options: {
+        outputPath: "public",
+      }
+    });
+    return config;
   }
 
 }
-
-module.exports = nextConfig
