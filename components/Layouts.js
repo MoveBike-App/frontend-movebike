42897b5fe779c01eff04dae7bc32ce4d1e659e24
jonatanare <jonatanare.dev@gmail.com>
2022-11-17 00:48:22 -0500

    [FEATURE]: add avatar progile, singout, login in app

--- components/Layouts.js
+++ components/Layouts.js
@@ -18,8 +18,28 @@ export default function Layouts ({ children, title, description }) {
           name='viewport'
           content='width=device-width, initial-scale=1.0'
         />
+        <title>{title}</title>
+        {/* <!-- Primary Meta Tags --> */}
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://movebike.mx" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="/assets/landing/movebike.jpg" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://movebike.mx" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content="/assets//landing/movebike.jpg" />
         <meta name='robots' content='follow' />
         <meta name='googlebot' content='index' />
+
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
       </Head>
