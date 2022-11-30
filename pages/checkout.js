44f7e654e972e11370ab685cb4fa0f7cf4273cdd
jonatanare <jonatanare.dev@gmail.com>
2022-11-30 12:25:51 -0500

    check if the cart is empty

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,21 +5,24 @@ import Layout from "components/Layouts";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { createReserve } from "services/reserves/reserve";
+import Link from "next/link";
 
 export default function Checkout() {
   const router = useRouter();
   const [priceCart, setPriceCart] = useState();
   const [motoCart, setMotoCart] = useState("");
-  const [idMoto, setIdMoto] = useState('')
+  const [idMoto, setIdMoto] = useState("");
+  const [checkout, setCheckout] = useState({});
   const getCart = () => {
     const cartStorage = JSON.parse(localStorage.getItem("cartCurrent"));
     if (cartStorage) {
+      setCheckout(cartStorage);
       const { vehicle, priceReserve, nameMoto } = cartStorage;
       setPriceCart(Number(priceReserve));
       setMotoCart(nameMoto);
-      setIdMoto(vehicle)
+      setIdMoto(vehicle);
     } else {
-      router.push("/");
+      //router.push("/");
     }
   };
 
@@ -41,91 +44,73 @@ export default function Checkout() {
           <div className="row">
             <div className="col-12">
               <h1 className="text-gray-600 mb-4">Finaliza tu reserva</h1>
-              <button onClick={handleReserve}>Reservar</button>
             </div>
             <div className="col-lg-7 mt-3">
               <section className="checkout__card">
                 <article className="checkout__card--article d-flex flex-column flex-md-row justify-content-center align-items-center">
-                  <Image
-                    src="/assets/landing/flotebikers/vitalia-125.webp"
-                    alt="Scooter Vitalia 125"
-                    width={130}
-                    height={95}
-                  />
-                  <div className="checkout__card-details text-justify">
-                    <span className="text-gray-400">Reservado x 1</span>
-                    <p className="mb-0 text-gray-400">Scooter WS Sport 150</p>
-                    <p className="mb-0 text-gray-400">
-                      <strong className="checkout__card--bold">
-                        Fecha Inicio:
-                      </strong>{" "}
-                      17-10-2022
-                    </p>
-                    <p className="mb-0 text-gray-400">
-                      <strong className="checkout__card--bold">
-                        Fecha Fin:
-                      </strong>{" "}
-                      25-10-2022
-                    </p>
-                    <p className="mb-0 text-gray-400">
-                      <strong className="checkout__card--bold">Día (s):</strong>{" "}
-                      5
-                    </p>
-                    <p className="mb-0 text-gray-400">
-                      <strong className="checkout__card--bold">Dónde:</strong>{" "}
-                      Cancún, Quintana Roo, Mex.
-                    </p>
-                  </div>
-                  <strong className="checkout__card-price text-orange-900 ms-md-auto">
-                    $2,700.00
-                  </strong>
-                </article>
-                <article className="checkout__card--article d-flex flex-column flex-md-row justify-content-center align-items-center">
-                  <Image
-                    src="/assets/landing/flotebikers/vitalia-150.webp"
-                    alt="Scooter Vitalia 125"
-                    width={130}
-                    height={95}
-                  />
-                  <div className="checkout__card-details text-justify">
-                    <span className="text-gray-400">Reservado x 1</span>
-                    <p className="mb-0 text-gray-400">Scooter WS Sport 150</p>
-                    <p className="mb-0 text-gray-400">
-                      <strong className="checkout__card--bold">
-                        Fecha Inicio:
-                      </strong>{" "}
-                      17-10-2022
-                    </p>
-                    <p className="mb-0 text-gray-400">
-                      <strong className="checkout__card--bold">
-                        Fecha Fin:
-                      </strong>{" "}
-                      25-10-2022
-                    </p>
-                    <p className="mb-0 text-gray-400">
-                      <strong className="checkout__card--bold">Día (s):</strong>{" "}
-                      5
-                    </p>
-                    <p className="mb-0 text-gray-400">
-                      <strong className="checkout__card--bold">Dónde:</strong>{" "}
-                      Cancún, Quintana Roo, Mex.
-                    </p>
-                  </div>
-                  <strong className="checkout__card-price text-orange-900 ms-md-auto">
-                    $2,700.00
-                  </strong>
+                  {(motoCart && (
+                    <>
+                      <Image
+                        src="/assets/landing/flotebikers/vitalia-125.webp"
+                        alt="Scooter Vitalia 125"
+                        width={130}
+                        height={95}
+                      />
+                      <div className="checkout__card-details text-justify">
+                        <span className="text-gray-400">Reservado x 1</span>
+                        <p className="mb-0 text-gray-400">
+                          {checkout.nameMoto}
+                        </p>
+                        <p className="mb-0 text-gray-400">
+                          <strong className="checkout__card--bold">
+                            Fecha Inicio:
+                          </strong>{" "}
+                          {checkout.fechaInical}
+                        </p>
+                        <p className="mb-0 text-gray-400">
+                          <strong className="checkout__card--bold">
+                            Fecha Fin:
+                          </strong>{" "}
+                          {checkout.fechaFinal}
+                        </p>
+                        <p className="mb-0 text-gray-400">
+                          <strong className="checkout__card--bold">
+                            Día (s):
+                          </strong>{" "}
+                          {checkout.dias}
+                        </p>
+                        <p className="mb-0 text-gray-400">
+                          <strong className="checkout__card--bold">
+                            Dónde:
+                          </strong>{" "}
+                          Cancún, Quintana Roo, Mex.
+                        </p>
+                      </div>
+                      <strong className="checkout__card-price text-orange-900 ms-md-auto">
+                        ${checkout.priceReserve}
+                      </strong>
+                    </>
+                  )) || (
+                    <div className="d-flex flex-column">
+                    <h3>Carrito vacío</h3>
+                    <Link href={'/motos'} className="btn btn-movebike contained mt-3">Ver motos</Link>
+                    </div>
+                  )}
                 </article>
               </section>
             </div>
-            <div className="col-lg-5 mt-3">
-              <section className="checkout__card mt-3">
-                <CheckoutCard
-                  price={Number(priceCart)}
-                  description={motoCart}
-                  vehicle={idMoto}
-                />
-              </section>
-            </div>
+            {(motoCart && (
+              <div className="col-lg-5 mt-3">
+                <section className="checkout__card mt-3">
+                  <CheckoutCard
+                    price={Number(priceCart)}
+                    description={motoCart}
+                    vehicle={idMoto}
+                  />
+                </section>
+              </div>
+            )) ||
+              null}
           </div>
         </div>
       </main>
